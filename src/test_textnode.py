a1ad43ac9cddd96e12ff642416914cfcb3627b5b
Greg Johnson <thisisgreg2.0@gmail.com>
2026-06-19 15:41:12 -0400

    implimented text_node_to_html_node method

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,5 @@
 import unittest
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -21,6 +21,12 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.TEXT, url="www.wowhead.com")
         node2 = TextNode("This is a text node", TextType.TEXT)
         self.assertNotEqual(node, node2)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
 
 if __name__ == "__main__":
     unittest.main()
