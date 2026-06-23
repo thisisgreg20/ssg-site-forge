eae75aeea4a3fb808a35d2cf80c2e9f832ed8957
Greg Johnson <thisisgreg2.0@gmail.com>
2026-06-23 16:41:26 -0400

    end of day push

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -27,3 +27,45 @@ def extract_markdown_links(text):
     pattern = r"(?<!\!)\[([^\[\]]+)\]\((.*?)\)"
     return re.findall(pattern, text)
     
+def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        images = extract_markdown_images(old_node.text)
+        if not images:
+            new_nodes.append(old_node)
+            continue
+        original_text = old_node.text
+        for image in images:
+            sections = original_text.split(f"![{image[0]}]({image[1]})", 1)
+            if sections[0]:
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(TextNode(image[0], TextType.IMAGE, image[1]))
+            original_text = sections[1]
+        if original_text:
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+    return new_nodes
+        
+
+def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        links = extract_markdown_links(old_node.text)
+        if not links:
+            new_nodes.append(old_node)
+            continue
+        original_text = old_node.text
+        for link in links:
+            sections = original_text.split(f"[{link[0]}]({link[1]})", 1)
+            if sections[0]:
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(TextNode(link[0], TextType.LINK, link[1]))
+            original_text = sections[1]
+        if original_text:
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+    return new_nodes
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,12 @@
 import unittest
 from textnode import TextNode, TextType
-from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
+from inline_markdown import (
+    split_nodes_delimiter, 
+    extract_markdown_images, 
+    extract_markdown_links, 
+    split_nodes_image, 
+    split_nodes_link
+)
 
 class TestInlineMarkdown(unittest.TestCase):
     def test_split_code(self):
@@ -56,6 +62,43 @@ class TestInlineMarkdown(unittest.TestCase):
         text = "Go to [About](/about-us)"
         matches = extract_markdown_links(text)
         self.assertEqual(matches, [("About", "/about-us")])
+
+    def test_split_image(self):
+        node = TextNode(
+            "This is text with an image ![boots](https://i.imgur.com/WZZ7L81.png) and another ![bear](https://i.imgur.com/fjrU4V1.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an image ", TextType.TEXT),
+                TextNode("boots", TextType.IMAGE, "https://i.imgur.com/WZZ7L81.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode("bear", TextType.IMAGE, "https://i.imgur.com/fjrU4V1.png"),
+            ],
+            new_nodes,
+        )
+
+    def test_split_link(self):
+        node = TextNode(
+            "Click [here](https://boot.dev) to learn coding!",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("Click ", TextType.TEXT),
+                TextNode("here", TextType.LINK, "https://boot.dev"),
+                TextNode(" to learn coding!", TextType.TEXT),
+            ],
+            new_nodes,
+        )
+
+    def test_split_no_links(self):
+        node = TextNode("Just plain text, no loot here.", TextType.TEXT)
+        new_nodes = split_nodes_link([node])
+        self.assertEqual(len(new_nodes), 1)
+        self.assertEqual(new_nodes[0].text, "Just plain text, no loot here.")
 
 if __name__ == "__main__":
     unittest.main()
