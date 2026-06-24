d81c814ef7ccc41fd676c61ed651cc27c94243f2
Greg Johnson <thisisgreg2.0@gmail.com>
2026-06-24 15:59:07 -0400

    end of workday push

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,7 @@
+from htmlnode import HTMLNode, ParentNode, LeafNode
+from textnode import TextNode, TextType
+from blocknode import BlockType, block_to_block_type
+
 def markdown_to_blocks(markdown):
     doc = markdown.split("\n\n")
     blocks = []
@@ -7,3 +11,9 @@ def markdown_to_blocks(markdown):
             continue
         blocks.append(block)
     return blocks
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    for block in blocks:
+        block_type = block_to_block_type(block)
+        
