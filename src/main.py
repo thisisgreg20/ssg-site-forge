077018bad6111dbfd81cde77ccb19e8d6dc27301
Greg Johnson <thisisgreg2.0@gmail.com>
2026-06-29 15:24:38 -0400

    added copy function

--- src/main.py
+++ src/main.py
@@ -1,7 +1,22 @@
-from textnode import TextNode, TextType
+import os
+import shutil
 
+def copy_src_to_destination(source: str = "./static", destination: str = "./public"):
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+    os.makedirs(destination)
+    for item in os.listdir(source):
+        source_path = os.path.join(source, item)
+        destination_path = os.path.join(destination, item)
+        if os.path.isdir(source_path):
+            print(f"Creating Directory: {destination_path}")
+            copy_src_to_destination(source_path, destination_path)
+        else:
+            print(f"Copying File: {destination_path}")
+            shutil.copy(source_path, destination_path)
+            
 def main() -> None:
-    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(node)
+    copy_src_to_destination()
 
-main()
+if __name__ == "__main__":
+    main()
