f1070d4283755753eb2b81b62dde42dd8b435983
Greg Johnson <thisisgreg2.0@gmail.com>
2026-06-30 14:11:56 -0400

    generate_page & generate_page_recursive now working

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,3 +1,7 @@
+import os
+from pathlib import Path
+from block_markdown import markdown_to_html_node
+
 def extract_title(markdown):
     lines = markdown.split("\n")
     for line in lines:
@@ -6,3 +10,39 @@ def extract_title(markdown):
             text = line.lstrip("#")
             return text.strip()
     raise Exception("no title found")
+
+def get_file_contents(path):
+    with open(path) as file:
+        return file.read()
+    
+def write_file_contents(path, contents):
+    if os.path.dirname(path) != "":
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+    with open(path, "w") as file:
+        file.write(contents)
+    
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    markdown = get_file_contents(from_path)
+    template = get_file_contents(template_path)
+    html_node = markdown_to_html_node(markdown)
+    title = extract_title(markdown)
+    html_content = html_node.to_html()
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html_content)
+    write_file_contents(dest_path, template)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    file_list = os.listdir(dir_path_content)
+    for file in file_list:
+        source_path = os.path.join(dir_path_content, file)
+        destination_path = os.path.join(dest_dir_path, file)
+        if os.path.isdir(source_path):
+            generate_pages_recursive(source_path, template_path, destination_path)
+            continue
+        if file[-3:] == ".md":
+            p = Path(destination_path)
+            destination_path = p.with_suffix(".html")
+            generate_page(source_path, template_path, destination_path)
+
+
--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from copystatic import copy_src_to_destination
+from gencontent import generate_pages_recursive
 
 def main() -> None:
     copy_src_to_destination()
+    generate_pages_recursive("content/", "template.html", "public/")
+    
 
 if __name__ == "__main__":
     main()
